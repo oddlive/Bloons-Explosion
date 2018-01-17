f9ac7f3f5c6a2e968c3437ae139d47d41921bd84
Alec <panayotov2325@students.d211.org>
2018-01-17 13:15:01 -0600

    g

--- app/src/main/java/com/example/rc211/myapplication/MainActivity.java
+++ app/src/main/java/com/example/rc211/myapplication/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean getNitin(boolean stupid){
         if (stupid){
             return true;
+            
         }
         return false;
     }
